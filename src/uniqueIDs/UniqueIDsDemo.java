954de7f3e355cdad4fe6749a72b7b1e0e7fe50b1
foudelsalhi <fadhelsalhif@gmail.com>
2016-11-20 19:06:43 +0100

    storing UUID object

--- src/uniqueIDs/UniqueIDsDemo.java
+++ src/uniqueIDs/UniqueIDsDemo.java
@@ -6,7 +6,6 @@ public class UniqueIDsDemo {
 	public static void main(String[] args) {
 
 		UniqueIDsImplementation unique = new UniqueIDsImplementation();
-		UUID uid = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
 		System.out.println("Clock sequence value: "+ unique.clockSequence());  
 	}
 }
