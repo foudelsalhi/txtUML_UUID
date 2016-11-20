6c506dbe537c9c17ffed77f765d98d402dc0850f
foudelsalhi <fadhelsalhif@gmail.com>
2016-11-20 14:33:01 +0100

    Renaming UniqueIDs.java file

--- src/uniqueIDs/UniqueIDsDemo.java
+++ src/uniqueIDs/UniqueIDsDemo.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 public class UniqueIDsDemo {
 	public static void main(String[] args) {
 		
-		UniqueIDs unique = new UniqueIDs();
+		UniqueIDsImplementation unique = new UniqueIDsImplementation();
 		UUID uid = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
 		System.out.println("Clock sequence value: "+ unique.clockSequence(uid)); 
 	}
