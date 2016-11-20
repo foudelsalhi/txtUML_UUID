1c2831c8e1e7a823cac10176590e8066149143bc
foudelsalhi <fadhelsalhif@gmail.com>
2016-11-20 19:04:44 +0100

    storing UUID object

--- src/uniqueIDs/UniqueIDsImplementation.java
+++ src/uniqueIDs/UniqueIDsImplementation.java
@@ -1,12 +1,23 @@
 package uniqueIDs;
 
-import java.util.UUID;
-
 public class UniqueIDsImplementation implements UniqueIDsInterface {
+	public UniqueIDsImplementation() {
+		super();
+	}
+
+	protected UniqueIDsImplementation(java.util.UUID val) {
+		this.var = val;
+	}
+
+	public UniqueIDsImplementation(long mostSigBits, long leastSigBits) {
+		this.var = new java.util.UUID(mostSigBits, leastSigBits);
+	}
+
+	private java.util.UUID var;
 
 	@Override
-	public int clockSequence(UUID clockSeq) {
-		return clockSeq.clockSequence();
+	public int clockSequence() {
+		return var.clockSequence();
 	}
 
 	@Override
--- src/uniqueIDs/UniqueIDsInterface.java
+++ src/uniqueIDs/UniqueIDsInterface.java
@@ -1,12 +1,11 @@
 package uniqueIDs;
 
-import java.util.UUID;
-
 import hu.elte.txtuml.api.model.external.ExternalClass;
 
 public interface UniqueIDsInterface extends ExternalClass {
 
-	public int clockSequence(UUID clockSeq);
+	public int clockSequence();
+
 	public String fromString();
 
 }
